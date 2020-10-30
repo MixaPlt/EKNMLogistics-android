40d40ad969d906556a6adfc091d4ab8a549b8468
Mykhailo Plotnikov <mixaplt@hotmail.com>
2020-10-30 12:08:34 +0200

    Fixed infinite end destination fragments opening - added ukrainian localization - minor UI improvements

--- app/src/main/java/net/eknm/eknmlogistics/order/OrderFlowFragment.kt
+++ app/src/main/java/net/eknm/eknmlogistics/order/OrderFlowFragment.kt
@@ -1,5 +1,7 @@
 package net.eknm.eknmlogistics.order
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import net.eknm.eknmlogistics.R
@@ -13,6 +15,11 @@ class OrderFlowFragment : BaseFlowFragment<OrderFlowViewModel>() {
         showFragment(DestinationFragment.newInstance(getString(R.string.select_start_destination)))
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        observeViewModel()
+    }
+
     override fun onAttachFragment(childFragment: Fragment) {
         super.onAttachFragment(childFragment)
         when (childFragment) {
@@ -20,9 +27,15 @@ class OrderFlowFragment : BaseFlowFragment<OrderFlowViewModel>() {
         }
     }
 
+    private fun observeViewModel() {
+        viewModel.showEndDestinationFragmentEvent.observe(viewLifecycleOwner, Observer {
+            showFragment(DestinationFragment.newInstance(getString(R.string.select_end_destination)))
+        })
+    }
+
     private fun observeDestinationsViewModel(destinationViewModel: DestinationViewModel) {
         destinationViewModel.destinationSelected.observe(this, Observer {
-            showFragment(DestinationFragment.newInstance(getString(R.string.select_end_destination)))
+            viewModel.onNewLocation(it)
         })
     }
 
--- app/src/main/java/net/eknm/eknmlogistics/order/OrderFlowViewModel.kt
+++ app/src/main/java/net/eknm/eknmlogistics/order/OrderFlowViewModel.kt
@@ -1,8 +1,25 @@
 package net.eknm.eknmlogistics.order
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import net.eknm.eknmlogistics.android.base.SingleLiveEvent
 import net.eknm.eknmlogistics.android.base.navigation.BaseFlowViewModel
+import net.eknm.eknmlogistics.mapInteraction.Location
 import javax.inject.Inject
 
 class OrderFlowViewModel @Inject constructor() : BaseFlowViewModel() {
+    private var startLocation: Location? = null
+    private var endLocation: Location? = null
 
+    private val _showEndDestinationFragmentEvent = SingleLiveEvent<Unit>()
+    val showEndDestinationFragmentEvent: LiveData<Unit> = _showEndDestinationFragmentEvent
+
+    fun onNewLocation(location: Location) {
+        if (startLocation == null) {
+            startLocation = location
+            _showEndDestinationFragmentEvent.call()
+        } else {
+            endLocation = location
+        }
+    }
 }
